abff6bd0fffc8418e87658d7ff5fc8bf738bcb44
RamProjector <ramyjustinefabi@gmail.com>
2024-11-15 12:54:15 +0800

    Update StringConcatenation.java

--- StringConcatenation.java
+++ StringConcatenation.java
@@ -7,14 +7,14 @@ public static String concatenateStrings (String str1, String str2) {
      char[] str3 = new char[len1 + len2];
      
      int i = 0;
-     while (1 < len1) { 
-        str3[1] = str1.charAt(i);
+     while (i < len1) { 
+        str3[i] = str1.charAt(i);
         i++;
      }
      
      int j = 0;
-     while (1 < len1 + len2) { 
-        str3[1] = str2.charAt(i);
+     while (i < len1 + len2) { 
+        str3[i] = str2.charAt(i);
         i++;
         j++;
      }
