89b0a526a5d6c68b5600d24c41b09771bc9fef14
RamProjector <ramyjustinefabi@gmail.com>
2025-02-14 15:09:02 +0800

    Update Task3.py

--- Activity2/Task3.py
+++ Activity2/Task3.py
@@ -1,23 +1,26 @@
 class Ba:
+    
     def __init__(self, an, o, b=0):
         self.an = an
         self.o = o
         self.b = b
 
     def deposit(self, am):
+        am = int(input("Enter Deposit: "))
         self.b += am
         print(f"Deposited ${am}. New balance: ${self.b}")
 
     def withdraw(self, am):
-        self.b -= am
-        print(f"Withdrew ${am}. New balance: ${self.b}")
+        am = int(input("Enter Withdrawal: "))
+        nb = self.b - am
+        print(f"Withdrew ${am}. New balance: ${nb}")
 
     def display_balance(self):
         print(f"Account balance for {self.o}: ${self.b}")
 
 
-acc = Ba("09947393341", "Fabi", 405999)
+acc = Ba("09947393341", "Fabi", 0)
 
-acc.deposit(6000)
-acc.withdraw(000)
+acc.deposit(0)
+acc.withdraw(0)
 acc.display_balance()
